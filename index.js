3bfc096af7ce6b9c42a02b166d383851ebc3be72
Anuj Shah <anujshah120102@gmail.com>
2024-03-14 12:36:18 +0530

    new code added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// console.log("Aditya Shah")
-let firstname="Aditya Shah";
+// console.log("Aditya Singh")
+// let firstname="Aditya Singh";
 
 // **strings are immutable, original string will not change
 // console.log(firstname.toUpperCase());
@@ -41,8 +41,32 @@ let firstname="Aditya Shah";
 // console.log(age);   // 33
 
 
-let str1="18";
-let str2="223";
+// let str1="18";
+// let str2="223";
 
-let newStr = +str1 + +str2;   
-console.log(newStr);      // 241
+// let newStr = +str1 + +str2;   
+// console.log(newStr);      // 241
+
+
+// **unbdefined null BigInt
+// let firstName;   // just declared a variable, no initialisation
+// var name;  // will also be undefined
+// const name; // will give error, must initialise
+// console.log(typeof firstName);   // undefined 
+
+// let firstname=null;
+// console.log(typeof firstname);   // object
+// this is a bug, should be null only, but cannot resolve now
+
+
+// let number=BigInt(123);    // this is constructor
+// let number2=BigInt(12323214343432434324324324345);
+// let number2=12323214343432434324324324345;   // will not be represented accurately as integer
+// let number3=123n;   // other way 
+// console.log(Number.MAX_SAFE_INTEGER);    //9007199254740991
+// console.log(typeof number2);    // bigint
+
+// console.log(123n + 123);    // error
+
+
+// **boolean and comparision operator
